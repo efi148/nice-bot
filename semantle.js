904714b6abf0b65f836939a550a6439b66159c83
Efi7 <89740773+Efi7@users.noreply.github.com>
2023-10-29 16:41:47 +0200

    Update semantle.js

--- semantle.js
+++ semantle.js
@@ -1,28 +1,25 @@
-import {getWordsList} from 'most-common-words-by-language';
-import {Subject} from 'rxjs';
+import { getWordsList } from 'most-common-words-by-language';
+import { Subject } from 'rxjs';
 import fetch from 'node-fetch';
 
 const lang = 'hebrew';
+const baseURL = 'https://semantle.ishefi.com';
 export const logging$ = new Subject();
 
 export async function getAllWords(wordsNum = 10000, sMinLimit = 40, sMaxLimit = 70) {
     function progress(total, current) {
-        const section = total / 10;
-        if (current % section == 0) {
-            const progress = current / section;
-            const empty = '_';
-            const full = '*';
-            const progressBar = `${current}/${total}\n[${full.repeat(progress)}${empty.repeat(10 - progress)}]`;
+        const fracion = total / 100;
+        if (current % fracion == 0) {
+            const progressBar = `${current * 100 / total}% passed...`;
             logging$.next(progressBar);
         }
-        // if (current % 10 == 0) logging$.next(`The word ${current}/${total} is being tested now.`);
     }
 
     function filter(array, similarityMinLimit, similarityMaxLimit, countLimit) {
         let newArr = array.sort((a, b) => b.similarity - a.similarity);
         newArr = newArr.filter(e => e.similarity > similarityMinLimit &&
-                                    e.similarity < similarityMaxLimit &&
-                                    e.similarity != null);
+            e.similarity < similarityMaxLimit &&
+            e.similarity != null);
         if (typeof countLimit !== 'undefined') {
             newArr = newArr.filter((e, i) => i < countLimit);
         }
@@ -31,16 +28,45 @@ export async function getAllWords(wordsNum = 10000, sMinLimit = 40, sMaxLimit = 
 
     let count = 1;
     const wordsList = getWordsList(lang, wordsNum);
-    let resultList = [];
-    for (const word of wordsList) {
-        await progress(wordsNum, count++);
-        resultList.push(await checkWord(word));
+    const resultList = [];
+    let wordsListIndex = 0;
+
+    function getSemantleWordsList() {
+        return new Promise((resolve, reject) => {
+            async function processNextWord() {
+                if (wordsListIndex < wordsList.length) {
+                    try {
+                        const currentWord = await checkWord(wordsList[wordsListIndex]);
+                        resultList.push(currentWord);
+                        wordsListIndex++;
+                        progress(wordsNum, count++);
+                        setTimeout(processNextWord, 2000);
+                    } catch (error) {
+                        reject(error);
+                    }
+                } else {
+                    logging$.next('DONE!');
+                    resolve(resultList);
+                }
+            }
+
+            processNextWord();
+        });
     }
 
-    return filter(resultList, sMinLimit, sMaxLimit);
+    try {
+        const returnData = await getSemantleWordsList().then(result => {
+            return filter(result, sMinLimit, sMaxLimit);
+        });
+
+        return returnData;
+    } catch (err) {
+        throw err;
+    }
+
 }
 
-export async function checkWord(wordToCheck) {
-    let result = await fetch(`https://semantle.ishefi.com/api/distance?word=${wordToCheck}`).then(res => res.json());
-    return {word: wordToCheck, similarity: result.similarity, distance: result.distance};
+async function checkWord(wordToCheck) {
+    let result = await fetch(`${baseURL}/api/distance?word=${wordToCheck}`).then(res => res.json());
+    return { word: wordToCheck, similarity: result.similarity, distance: result.distance };
 }
